4323468f06d6e0ae2518e261c2bfed864b4fdccf
patybn3 <paty.nigro@gmail.com>
2021-02-12 16:22:57 -0500

    AWS Amplify

--- server/routes/item-router.js
+++ server/routes/item-router.js
@@ -4,7 +4,7 @@ const ItemController = require('../controllers/item-controller');
 
 const router = express.Router();
 
-router.get('/books', ItemController.getItems);
+router.get('/library', ItemController.getItems);
 router.get('/book/:id', ItemController.getItemById);
 router.post('/book', ItemController.createItem);
 router.put('/book/:id', ItemController.updateItem);
